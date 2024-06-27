b17f465566671c1309823bf08586d4cacedcd7e7
yaasiinaxmed <yaskassoy@gmail.com>
2024-06-27 22:54:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hell from ts");
+var myName = "Hello";
+function add() {
+    console.log("Hello from add function");
+}
+function greeting() {
+    console.log("Hey Typescript");
+}
+add();
+greeting();
---
+++ index.ts
@@ -0,0 +1,14 @@
+console.log("Hell from ts");
+
+let myName = "Hello";
+
+function add() {
+    console.log("Hello from add function")
+}
+
+function greeting() {
+    console.log("Hey Typescript")
+}
+
+add()
+greeting();
