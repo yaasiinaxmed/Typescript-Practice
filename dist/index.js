df67777e3c76e4b2ff394aa6c9f883e68eee970d
yaasiinaxmed <yaskassoy@gmail.com>
2024-06-27 23:02:42 +0300

    updated

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Hell from ts");
+let myName = "Hello";
+function add() {
+    console.log("Hello from add function");
+}
+function greeting() {
+    console.log("Hey Typescript");
+}
+add();
+greeting();
