13aa6e1c2337dd897ea4d28b2bbc42d5ed622469
yaasiinaxmed <yaskassoy@gmail.com>
2024-06-27 23:05:53 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ console.log("Hell from ts");
 let myName = "Hello";
 
 function add() {
-    console.log("Hello from add function")
+    console.log("Hello from add function ❤️")
 }
 
 function greeting() {
